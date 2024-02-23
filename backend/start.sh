c7b6b4fe845250678fe98c85fe9730c74a3e6bd1
juanmggb <juanmanuelgutierrezgarcia@outlook.com>
2024-02-23 12:59:45 -0600

    fourth commit

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+python manage.py makemigrations element
 python manage.py makemigrations users
-python manage.py makemigrations 
+python manage.py makemigrations experiment
+# python manage.py makemigrations 
 python manage.py migrate --no-input
 # python manage.py runserver 0.0.0.0:8000
 python manage.py collectstatic --no-input
